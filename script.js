8cf8de002b5dd2e5557ef44f5f48ff9ccce00ae0
KallNascimento <iksn7@icloud.com>
2022-02-27 11:15:40 -0300

    Movimentando a cobra

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("game");
 let context = canvas.getContext("2d");
 let box = 32;
+let direction = "right";
 let snake = [];
 snake[0] = {
     x: 8 * box,
@@ -20,5 +21,27 @@ function createPlayer() {
     }
 }
 
-createBackground();
-createPlayer();
+function gameStart() {
+    createBackground();
+    createPlayer();
+    let snakeY = snake[0].y;
+    let snakeX = snake[0].x;
+
+    if (direction === "right") snakeX += box;
+    if (direction === "left") snakeX -= box;
+    if (direction === "up") snakeY -= box;
+    if (direction === "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+
+}
+
+let game = setInterval(gameStart(), 100);
