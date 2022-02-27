59373f8d7db38ad348392edcccb3f0c4d64e8b90
KallNascimento <iksn7@icloud.com>
2022-02-27 11:00:55 -0300

    Criando player

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById("game");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 
-function createBackground(){
-context.fillStyle = "#4FD3C4"
-context.fillRect(0,0, 16 * box, 16 * box) //X,y Position, widht and height
 }
 
-createBackground()
+function createBackground() {
+    context.fillStyle = "#4FD3C4"
+    context.fillRect(0, 0, 16 * box, 16 * box) //X,y Position, widht and height
+}
+
+function createPlayer() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "#1C0A00";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+createBackground();
+createPlayer();
